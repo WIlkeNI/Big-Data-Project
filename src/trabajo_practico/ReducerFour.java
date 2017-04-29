d6dce77270ab6d28c0b1b2a6db308ad9f9a1531d
Juani Harguindeguy <jiharguindeguy@gmail.com>
2017-04-29 20:17:28 -0300

    Agregando tablas de valores al reducerJob 4

---
+++ src/trabajo_practico/ReducerFour.java
@@ -0,0 +1,33 @@
+package tp;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class ReducerFour extends Reducer<LongWritable, Text, LongWritable, Text/*LongWritable*//*Deberia devolver un text con las columnas concatenadas*/> {
+
+	public void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+		Map<Integer, Integer> bonusPersonales = new HashMap<Integer, Integer>();
+    bonusPersonales.put(500, 2);		bonusPersonales.put(251, 1.6);
+    bonusPersonales.put(51, 1.3);		bonusPersonales.put(11, 1.1);
+    bonusPersonales.put(1, 1.02);	  bonusPersonales.put(0, 1);
+
+    Map<Integer, Integer> bonusDepartamento = new HashMap<Integer, Integer>();
+    bonusDepartamento.put(1, 1.35);   bonusDepartamento.put(2, 1,3);
+    bonusDepartamento.put(3, 1.25);   bonusDepartamento.put(4, 1.2);
+    bonusDepartamento.put(4, 1.15);   bonusDepartamento.put(6, 1.1);
+
+    for (@SuppressWarnings("unused") Object val : values) {
+			String[] campos = val.toString().split("\t");
+			importe += Double.parseDouble(campos[1]);
+			ventas += Integer.parseInt(campos[0]);
+		}
+
+		context.write(key, new Text(ventas + "\t" + String.format("%f",importe)));
+	}
+
+}
